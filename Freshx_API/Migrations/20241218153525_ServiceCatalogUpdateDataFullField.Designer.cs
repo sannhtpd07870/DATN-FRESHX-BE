209774a7ded0e542a8a425b37b3d1239926531de
NhatTimo <nguyenvannhatltqb@gmail.com>
2024-12-18 22:44:05 +0700

    Update migration model ServiceCatalog

--- Freshx_API/Migrations/20241218153525_ServiceCatalogUpdateDataFullField.Designer.cs
+++ Freshx_API/Migrations/20241218153525_ServiceCatalogUpdateDataFullField.Designer.cs
@@ -4,6 +4,7 @@ using Freshx_API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Freshx_API.Migrations
 {
     [DbContext(typeof(FreshxDBContext))]
-    partial class FreshxDBContextModelSnapshot : ModelSnapshot
+    [Migration("20241218153525_ServiceCatalogUpdateDataFullField")]
+    partial class ServiceCatalogUpdateDataFullField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
