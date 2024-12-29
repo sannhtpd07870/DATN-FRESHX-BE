6fd6e906a96a625665afe404df1e101dec27dca7
Vanth <vanth@example.com>
2024-12-29 17:06:51 +0700

    fix bug unitOfMeasures

--- Freshx_API/Repository/UnitOfMeasure/UnitOfMeasure.cs
+++ Freshx_API/Repository/UnitOfMeasure/UnitOfMeasure.cs
@@ -63,6 +63,34 @@ namespace Freshx_API.Repository
                 .FirstOrDefaultAsync(u => u.UnitOfMeasureId == id && (u.IsDeleted == 0 || u.IsDeleted == null));
         }
 
+        public async Task<UnitOfMeasure?> GetByCodeAsync(string code)
+        {
+            try
+            {
+                return await _context.UnitOfMeasures
+                    .FirstOrDefaultAsync(s => s.Code == code && (s.IsDeleted == 0 || s.IsDeleted == null));
+            }
+            catch (Exception ex)
+            {
+                // Log exception hoặc xử lý thêm
+                throw new Exception("Error retrieving UnitOfMeasure", ex);
+            }
+        }
+
+        public async Task<UnitOfMeasure?> GetNameAsync(string name)
+        {
+            try
+            {
+                return await _context.UnitOfMeasures
+                    .FirstOrDefaultAsync(s => s.Name == name && (s.IsDeleted == 0 || s.IsDeleted == null));
+            }
+            catch (Exception ex)
+            {
+                // Log exception hoặc xử lý thêm
+                throw new Exception("Error retrieving UnitOfMeasure", ex);
+            }
+        }
+
         // Tạo mới đơn vị đo lường
         public async Task<UnitOfMeasure> CreateAsync(UnitOfMeasure entity)
         {
@@ -87,6 +115,24 @@ namespace Freshx_API.Repository
                 entity.IsDeleted = 1;
                 await _context.SaveChangesAsync();
             }
+        }
+
+        public async Task DeleteAsyncCode(string code)
+        {
+            // Tìm nhà cung cấp theo code
+            var entity = await _context.UnitOfMeasures.FirstOrDefaultAsync(s => s.Code == code && (s.IsDeleted == null || s.IsDeleted == 0));
+
+            if (entity == null)
+            {
+                // Nếu không tìm thấy, ném ngoại lệ hoặc xử lý theo cách khác
+                throw new KeyNotFoundException("Đơn vị đo lường không tồn tại hoặc đã bị xóa.");
+            }
+
+            // Đánh dấu là đã xóa (soft delete) thay vì xóa thật
+            entity.IsDeleted = 1; // Hoặc bạn có thể set IsDeleted = true tùy thuộc vào kiểu dữ liệu của bạn
+
+            // Lưu thay đổi vào cơ sở dữ liệu
+            await _context.SaveChangesAsync();
         }
     }
 }
